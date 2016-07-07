729db437545ab027c19e6578732cba02e712142e
Vladislav Kochemaev <vladislav.kochemaev@gmail.com>
2016-07-07 11:53:52 +0400

    CFSIReader

---
+++ CFSIReader/CFSIReader/BinaryHelper.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.IO;
+
+namespace CFSIReader
+{
+    public class BinaryHelper
+    {
+        public static bool bUseUnicode = false;
+
+        public Int32 ReadInt32(Stream fs)
+        {
+            byte[] buf = new byte[4];
+            fs.Read(buf, 0, 4);
+            return BitConverter.ToInt32(buf, 0);
+        }
+
+        public string ReadString(Stream fs)
+        {
+            int nLength = ReadInt32(fs);
+            if (nLength > 0 && !bUseUnicode)
+            {
+                if (nLength > 10000)
+                    throw new Exception("Probably bad read!");
+                byte[] buf = new byte[nLength];
+                fs.Read(buf, 0, nLength);
+                return System.Text.Encoding.ASCII.GetString(buf, 0, nLength - 1);
+            }
+            else if (nLength < 0)
+            {
+                nLength = -nLength * 2;
+                if (nLength > 20000)
+                    throw new Exception("Probably bad read!");
+                byte[] buf = new byte[nLength];
+                fs.Read(buf, 0, nLength);
+                return System.Text.Encoding.Unicode.GetString(buf, 0, nLength - 2);
+            }
+            return "";
+        }
+
+        public UInt32 ReadUInt32(Stream fs)
+        {
+            byte[] buf = new byte[4];
+            fs.Read(buf, 0, 4);
+            return BitConverter.ToUInt32(buf, 0);
+        }
+
+        public Guid ReadGuid(Stream fs)
+        {
+            byte[] buf = new byte[16];
+            fs.Read(buf, 0, 16);
+            return new Guid(buf);
+        }
+
+        public Int16 ReadInt16(Stream fs)
+        {
+            byte[] buf = new byte[2];
+            fs.Read(buf, 0, 2);
+            return BitConverter.ToInt16(buf, 0);
+        }
+
+        public Int64 ReadInt64(Stream fs)
+        {
+            byte[] buf = new byte[8];
+            fs.Read(buf, 0, 8);
+            return BitConverter.ToInt64(buf, 0);
+        }
+
+        public byte ReadByte(Stream fs)
+        {
+            int value = fs.ReadByte();
+            return (byte)value;
+        }
+
+        public bool ReadBool(Stream fs)
+        {
+            UInt32 value = ReadUInt32(fs);
+            if (value == 0)
+                return false;
+            return true;
+        }
+
+        public UInt64 ReadUInt64(Stream fs)
+        {
+            byte[] buf = new byte[8];
+            fs.Read(buf, 0, 8);
+            return BitConverter.ToUInt64(buf, 0);
+        }
+
+        public float ReadFloat(Stream fs)
+        {
+            byte[] buf = new byte[4];
+            fs.Read(buf, 0, 4);
+            return BitConverter.ToSingle(buf, 0); ;
+        }
+
+        public UInt16 ReadUInt16(Stream fs)
+        {
+            byte[] buf = new byte[2];
+            fs.Read(buf, 0, 2);
+            return BitConverter.ToUInt16(buf, 0);
+        }
+
+        public void WriteInt32(Stream fs, Int32 Value)
+        {
+            byte[] buf = BitConverter.GetBytes(Value);
+            fs.Write(buf, 0, buf.Length);
+        }
+
+        public void WriteString(Stream fs, string Value)
+        {
+            if (Value.Length == 0)
+            {
+                WriteInt32(fs, 0);
+                return;
+            }
+            bool bNeedUnicode = false;
+            if (bUseUnicode)
+            {
+                bNeedUnicode = true;
+            }
+            else
+            {
+                for (int i = 0; i < Value.Length; i++)
+                {
+                    if (!('\u0000' <= Value[i] && Value[i] <= '\u00FF'))
+                    {
+                        bNeedUnicode = true;
+                        break;
+                    }
+                }
+            }
+            if (bNeedUnicode)
+            {
+                int nLength = -(Value.Length + 1);
+                WriteInt32(fs, nLength);
+                byte[] buf = System.Text.Encoding.Unicode.GetBytes(Value);
+                fs.Write(buf, 0, buf.Length);
+                WriteInt16(fs, 0);
+            }
+            else
+            {
+                int nLength = Value.Length + 1;
+                WriteInt32(fs, nLength);
+                byte[] buf = System.Text.Encoding.ASCII.GetBytes(Value);
+                fs.Write(buf, 0, buf.Length);
+                WriteByte(fs, 0);
+            }
+        }
+
+        public void WriteInt16(Stream fs, Int16 Value)
+        {
+            byte[] buf = BitConverter.GetBytes(Value);
+            fs.Write(buf, 0, buf.Length);
+        }
+
+        public void WriteByte(Stream fs, byte Value)
+        {
+            fs.WriteByte(Value);
+        }
+
+        public void WriteUInt32(Stream fs, UInt32 Value)
+        {
+            byte[] buf = BitConverter.GetBytes(Value);
+            fs.Write(buf, 0, buf.Length);
+        }
+
+        public void WriteGuid(Stream fs, Guid Value)
+        {
+            byte[] buf = Value.ToByteArray();
+            fs.Write(buf, 0, buf.Length);
+        }
+
+        public void WriteInt64(Stream fs, Int64 Value)
+        {
+            byte[] buf = BitConverter.GetBytes(Value);
+            fs.Write(buf, 0, buf.Length);
+        }
+
+        public void WriteBool(Stream fs, bool Value)
+        {
+            if (Value)
+                WriteUInt32(fs, 1);
+            else
+                WriteUInt32(fs, 0);
+        }
+
+        public void WriteFloat(Stream fs, float Value)
+        {
+            byte[] buf = BitConverter.GetBytes(Value);
+            fs.Write(buf, 0, 4);
+        }
+
+        public void WriteUInt16(Stream fs, UInt16 Value)
+        {
+            byte[] buf = BitConverter.GetBytes(Value);
+            fs.Write(buf, 0, buf.Length);
+        }
+    }
+}
---
+++ CFSIReader/CFSIReader/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFSIReader
+{
+    public class CFSI : BinaryHelper
+    {
+        public class FileInfo
+        {
+            public string m_sFileName;
+            public UInt32 m_nOffset;
+            public UInt32 m_nLength;
+        }
+
+        public List<FileInfo> m_tFiles;
+
+        public CFSI()
+        {
+            m_tFiles = new List<FileInfo>();
+        }
+
+        public void DeSerialize(Stream s)
+        {
+            Console.WriteLine("Reading files table...");
+            byte nCount = 0;
+            while (nCount != 1)
+                nCount = ReadByte(s); // ?
+            while (nCount > 0)
+            {
+                byte nDirNameLength = ReadByte(s);
+                byte[] pDirName = new byte[nDirNameLength];
+                s.Read(pDirName, 0, pDirName.Length);
+                string sDirName = ASCIIEncoding.ASCII.GetString(pDirName);
+                nCount = ReadByte(s);
+                for (int i = 0; i < nCount; i++)
+                {
+                    byte nFileNameLength = ReadByte(s);
+                    byte[] pFileName = new byte[nFileNameLength];
+                    s.Read(pFileName, 0, pFileName.Length);
+                    string sFileName = ASCIIEncoding.ASCII.GetString(pFileName);
+                    UInt32 nOffset = ReadUInt32(s);
+                    UInt32 nLength = ReadUInt32(s);
+                    m_tFiles.Add(new FileInfo() {
+                        m_sFileName = sDirName + sFileName,
+                        m_nOffset = nOffset << 4,
+                        m_nLength = nLength
+                    });
+                }
+            }
+            while (s.Position % 16 != 0)
+            {
+                nCount = ReadByte(s);
+                if (nCount != 0)
+                    throw new Exception("Bad read!");
+            }
+        }
+
+        public void Unpack(Stream s, string sDir)
+        {
+            UInt32 nStart = (UInt32)s.Position;
+
+            for (int i = 0; i < m_tFiles.Count; i++)
+            {
+                string sFullPath = Path.Combine(sDir, m_tFiles[i].m_sFileName);
+                Console.WriteLine("[" + (i + 1) + " of " + m_tFiles.Count + "] " + m_tFiles[i].m_sFileName);
+                Directory.CreateDirectory(Path.GetDirectoryName(sFullPath));
+                Stream fs = File.Open(sFullPath, FileMode.Create);
+                byte[] pData = new byte[m_tFiles[i].m_nLength];
+                while (s.Position != m_tFiles[i].m_nOffset + nStart)
+                {
+                    byte nTemp = ReadByte(s);
+                    if (nTemp != 0)
+                        throw new Exception("Bad read!");
+                }
+                s.Read(pData, 0, pData.Length);
+                fs.Write(pData, 0, pData.Length);
+                fs.Close();
+            }
+        }
+
+        public void Pack(Stream s, string sDir, Dictionary<string, List<string>> files)
+        {
+            if (false)
+                WriteUInt16(s, 0xe8fc); // priority?
+            WriteByte(s, 1);
+            UInt32 nCurrentOffset = 0;
+            foreach (var item in files)
+            {
+                string path = item.Key.Substring(sDir.Length);
+                if (path[0] == '\\')
+                    path = path.Substring(1);
+                if (path != "")
+                    path += "\\";
+                byte[] pDirName = ASCIIEncoding.ASCII.GetBytes(path.Replace('\\', '/'));
+                WriteByte(s, (byte)pDirName.Length);
+                s.Write(pDirName, 0, pDirName.Length);
+                WriteByte(s, (byte)item.Value.Count);
+                for (int i = 0; i < item.Value.Count; i++)
+                {
+                    byte[] pFileName = ASCIIEncoding.ASCII.GetBytes(item.Value[i].Replace('\\', '/'));
+                    WriteByte(s, (byte)pFileName.Length);
+                    s.Write(pFileName, 0, pFileName.Length);
+                    Stream fs = File.Open(Path.Combine(item.Key, item.Value[i]), FileMode.Open);
+                    UInt32 nLength = (UInt32)fs.Length;
+                    fs.Close();
+                    WriteUInt32(s, nCurrentOffset >> 4);
+                    WriteUInt32(s, nLength);
+                    m_tFiles.Add(new FileInfo()
+                    {
+                        m_sFileName = path + item.Value[i],
+                        m_nOffset = nCurrentOffset,
+                        m_nLength = nLength
+                    });
+                    nCurrentOffset += nLength + (nLength % 16 != 0 ? 16 - nLength % 16 : 0);
+                }
+            }
+            while (s.Position % 16 != 0)
+                WriteByte(s, 0);
+            UInt32 nStart = (UInt32)s.Position;
+            for (int i = 0; i < m_tFiles.Count; i++)
+            {
+                while (s.Position < nStart + m_tFiles[i].m_nOffset)
+                    WriteByte(s, 0);
+                Stream fs = File.Open(Path.Combine(sDir, m_tFiles[i].m_sFileName), FileMode.Open);
+                byte[] pData = new byte[m_tFiles[i].m_nLength];
+                fs.Read(pData, 0, pData.Length);
+                s.Write(pData, 0, pData.Length);
+                fs.Close();
+            }
+            while (s.Position % 16 != 0)
+                WriteByte(s, 0);
+        }
+    }
+
+    class Program
+    {
+        public static void Unpack(string filename, string out_path)
+        {
+            // unpack "E:\SteamLibrary\steamapps\common\Zero Escape\00000000.cfsi" "E:\SteamLibrary\steamapps\common\Zero Escape\00000000"
+            // unpack "E:\SteamLibrary\steamapps\common\Zero Escape\00000001.cfsi" "E:\SteamLibrary\steamapps\common\Zero Escape\00000002"
+            Stream fs = File.Open(filename, FileMode.Open);
+            CFSI cfsi = new CFSI();
+            cfsi.DeSerialize(fs);
+            cfsi.Unpack(fs, out_path);
+            fs.Close();
+        }
+
+        public static void GetFiles(string dir, ref Dictionary<string, List<string>> files)
+        {
+            string[] tFiles = Directory.GetFiles(dir);
+            if (tFiles.Length > 0)
+            {
+                files.Add(dir, new List<string>());
+                for (int i = 0; i < tFiles.Length; i++)
+                    files[dir].Add(Path.GetFileName(tFiles[i]));
+            }
+            string[] tDirs = Directory.GetDirectories(dir);
+            for (int i = 0; i < tDirs.Length; i++)
+                GetFiles(tDirs[i], ref files);
+        }
+
+        public static void Pack(string filename, string files_path)
+        {
+            // pack "E:\SteamLibrary\steamapps\common\Zero Escape\00000001.cfsi" "E:\SteamLibrary\steamapps\common\Zero Escape\00000001"
+            // pack "E:\SteamLibrary\steamapps\common\Zero Escape\00000001.cfsi" "E:\SteamLibrary\steamapps\common\Zero Escape\00000001"
+            files_path = Path.GetFullPath(files_path);
+            Dictionary<string, List<string>> m_mapFiles = new Dictionary<string, List<string>>();
+            GetFiles(files_path, ref m_mapFiles);
+            Stream fs = File.Open(filename, FileMode.Create);
+            CFSI cfsi = new CFSI();
+            cfsi.Pack(fs, files_path, m_mapFiles);
+            fs.Close();
+        }
+
+        static void Main(string[] args)
+        {
+            if (args[0] == "unpack")
+                Unpack(args[1], args[2]);
+            else if (args[0] == "pack")
+                Pack(args[1], args[2]);
+        }
+    }
+}
